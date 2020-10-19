dc782ebf60560bad787c94b19e14ddca28863156
Marvin <marvinegoavilz@gmail.com>
2020-10-19 02:59:07 +0100

    segundo comió

--- TablasWebView/ViewController.swift
+++ TablasWebView/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
+struct equipos {
+    let nombre : String!
+    let url: String!
+}
+
+class equiposCell:UITableViewCell {
+    @IBOutlet var labelName: UILabel!
+    @IBOutlet var buttonURL: UIButton!
+    
+}
+    
+
+
 class ViewController: UIViewController {
 
+    let arrayEquipos = [equipos(nombre:"Barcelona", url:"https://fcbarcelona.es" ),equipos(nombre:"Real Madrid", url:"https://realmadrid.com" ),equipos(nombre:"Getafe", url:"https://getafecf.com" ), equipos(nombre:"Rayo Vallecano", url:"https://rayovallecano.es" ) ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+ 
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+}
